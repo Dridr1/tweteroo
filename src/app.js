b9c1960267466b966b936c418df1af7fb6428e86
Dridr1 <a.driano@outlook.com>
2022-01-21 01:19:35 -0300

    reduced code size

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 import express from "express";
 import cors from 'cors';
 
-let user = {
-    username: "",
-    avatar: ""
-};
-
-const tweets = [];
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+let user = { username: "", avatar: "" };
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     user = req.body;
@@ -19,21 +14,17 @@ app.post('/sign-up', (req, res) => {
 });
 
 app.post('/tweets', (req, res) => {
-    tweets.unshift({...req.body, avatar: user.avatar});
+    tweets.unshift({ ...req.body, avatar: user.avatar });
     res.send('OK');
 });
 
 app.get('/tweets', (req, res) => {
-    if(tweets.length < 10) res.send(tweets);
-    else{
-        const tweetsTemp = [...tweets];
-        const lastTweets = tweetsTemp.slice(0, 10);
-        res.send(lastTweets);
-    }
+    if (tweets.length < 10) res.send(tweets);
+    else res.send(tweets.slice(0, 10));
 });
 
 app.listen(5000, () => {
     console.log('-------------------------------------');
     console.log('| Running at http://localhost:5000/ |');
     console.log('-------------------------------------');
-})
+});
