1edfe862381c91230706c7a4269f9cd86b4e7622
Dridr1 <a.driano@outlook.com>
2022-01-20 23:40:33 -0300

    added sign-up post request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
 
+let user;
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/sign-up', (req, res) => {
+    user = req.body;
+    res.send('OK');
+    console.log(user);
+})
 
 app.listen(5000, () => {
     console.log('-------------------------------------');
